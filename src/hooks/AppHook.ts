c8060279432019e01f5cce9d29e4628efb72b69f
gianynx <minusgian@gmail.com>
2025-01-17 12:52:19 +0100

    fix: update AppHook.ts

--- src/hooks/AppHook.ts
+++ src/hooks/AppHook.ts
@@ -1,20 +1,16 @@
-function AppHook() {
-    const appHookData = () => {
-        const h1Title = "React Training";
-        const h1Style = {
-            color: "black",
-            fontSize: "3.3rem",
-        };
-        const h2Title = "Full-time Course";
-        const boxPosition = 2;
-        const img = 'react';
+import AppHookData from '../types/AppHookTypes';
 
-        return { h1Title, h1Style, h2Title, boxPosition, img };
-    }
+const useAppHook = (): AppHookData => {
+    const h1Title = "React Training";
+    const h1Style = {
+        color: "black",
+        fontSize: "3.3rem",
+    };
+    const h2Title = "Full-time Course";
+    const boxPosition = 2;
+    const img = 'react';
 
-    return {
-        appHookData
-    }
+    return { h1Title, h1Style, h2Title, boxPosition, img };
 }
 
-export default AppHook;
+export default useAppHook;
