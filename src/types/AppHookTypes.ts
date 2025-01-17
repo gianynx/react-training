7b105c2588359f3235502c847aaca0ffbaf248d5
gianynx <minusgian@gmail.com>
2025-01-17 12:51:28 +0100

    feat: add AppHookTypes.ts

---
+++ src/types/AppHookTypes.ts
@@ -0,0 +1,9 @@
+export interface AppHookData {
+    h1Title: string;
+    h1Style: React.CSSProperties;
+    h2Title: string;
+    boxPosition: number;
+    img: string;
+}
+
+export default AppHookData;
