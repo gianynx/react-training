3219b5ef33067164396c6a87f0172f3907ddc9db
gianynx <minusgian@gmail.com>
2025-01-20 13:00:09 +0100

    feat: improve condition isPopular

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
-function Card({ title, imgSrc, children, isVisited }) {
+function Card({ title, imgSrc, children, isPopular }) {
+    const popularLabel = isPopular ? <span className="text-success">&#10003; Popular</span> : <span className="text-danger mx-3">&#10007; Popular</span>
 
     return (
         <div className="card" style={{ width: "18rem", height: "33rem" }}>
             <div className="d-flex justify-content-center align-items-center" style={{ height: "12rem" }}>
-                <img src={imgSrc} className="card-img-top" alt={title} style={{ width: "12rem" }} />
+                <img src={imgSrc} className="card-img-top" alt={title} style={{ width: "12rem", height: "12rem" }} />
             </div>
             <button className="btn btn-dark mt-4">Click me!</button>
             <div className="d-flex justify-content-center mt-4">
-                {isVisited ? <span className="text-success">&#10003; Visited</span> : <span className="text-danger mx-3">&#10007; Visited</span>}
+                {popularLabel}
             </div>
             <div className="card-body text-center">
                 <h5 className="card-title fw-bold">{title}</h5>
