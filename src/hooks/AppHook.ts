2bd0f59c04caba77028ebeed1aae799dfab2e1fa
gianynx <minusgian@gmail.com>
2025-01-20 17:59:04 +0100

    feat: add array technologies

--- src/hooks/AppHook.ts
+++ src/hooks/AppHook.ts
@@ -10,7 +10,31 @@ const useAppHook = (): AppHookData => {
     const boxPosition = 2;
     const img = 'react';
 
-    return { h1Title, h1Style, h2Title, boxPosition, img };
+    const technologies = [
+        {
+            id: "react",
+            title: "React logo",
+            imgSrc: "/react.svg",
+            isPopular: true,
+            description: "React is a JavaScript library for building user interfaces.",
+        },
+        {
+            id: "vite",
+            title: "Vite logo",
+            imgSrc: "/vite.svg",
+            isPopular: true,
+            description: "Vite is a build tool that manages the development and build process (&quot;engine&quot;) of a web application.",
+        },
+        {
+            id: "cobol",
+            title: "Cobol logo",
+            imgSrc: "/cobol.svg",
+            isPopular: false,
+            description: "COBOL is a historical programming language, designed for commercial and administrative applications.",
+        },
+    ];
+
+    return { h1Title, h1Style, h2Title, boxPosition, img, technologies };
 }
 
 export default useAppHook;
