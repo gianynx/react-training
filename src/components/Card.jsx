548621972ddbe9a8e84d72c6e5de96cd8a287198
gianynx <minusgian@gmail.com>
2025-01-23 18:22:06 +0100

    feat: update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,17 @@ function Card({ title, imgSrc, children, isPopular, isJsFramework }) {
 
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <div className="d-flex justify-content-center align-items-center" style={{ height: "12rem" }}>
+            <div className="d-flex justify-content-center align-items-center mb-3" style={{ height: "12rem" }}>
                 <img src={imgSrc} className="card-img-top" alt={title} />
             </div>
+            <h5 className="card-title fw-bold text-center mt-3">{title}</h5>
             <button className="btn btn-dark mt-4">Click me!</button>
             <div className="text-center mt-4">
                 <p className="fw-bold">{popularLabel}</p>
                 <p className="fw-bold">{jsFrameworkLabel}</p>
             </div>
+            <hr />
             <div className="card-body text-center p-2">
-                <h5 className="card-title fw-bold">{title}</h5>
                 <p className="card-text">{children}</p>
             </div>
         </div>
