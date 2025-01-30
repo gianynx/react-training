3a6ea6116d8b2d0c3c73147078b75c19299b8842
gianynx <minusgian@gmail.com>
2025-01-30 12:02:03 +0100

    feat: update h1 style

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
                 </a>
               </div>
               <div>
-                <h1 className="text-white">Vite + React</h1>
+                <h1>Vite + React</h1>
                 <div className="card">
                   <button onClick={() => setCount((count) => count + 1)}>Count is {count}</button>
                 </div>
