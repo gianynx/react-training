2ec6d3fe2ca000875480eb837e998805b25edf2d
gianynx <minusgian@gmail.com>
2025-01-30 11:49:09 +0100

    feat: update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer';
 import './assets/css/App.css';
 
 function App() {
+
   const [count, setCount] = useState(0);
   const { h1Title, h1Style, h2Title, boxPosition, img, technologies, handleSubmit, handleChange } = useAppHook();
 
