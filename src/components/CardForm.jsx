bc299e7f911df34c49e1d0cd559418ead3177ba7
gianynx <minusgian@gmail.com>
2025-02-07 12:55:31 +0100

    feat: add form

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -21,10 +21,32 @@ function CardForm({ addTechnology, removeTechnology }) {
     }
 
     return (
-        <>
-            <button type="button" className="btn btn-outline-success px-5" onClick={handleAdd}>Add</button>
-            <button type="button" className="btn btn-outline-danger px-5" onClick={handleDelete}>Delete</button>
-        </>
+        <form onSubmit={handleAdd} className="rounded p-5" style={{ backgroundColor: "#162638" }}>
+            <div className="d-flex align-items-center gap-4">
+                <div className="mb-3">
+                    <label htmlFor="inputName" className="form-label text-white">Title</label>
+                    <input type="text" id="inputName" name="inputName" className="form-control" placeholder="Enter new name" minLength="3" maxLength="23" required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="inputImg" className="form-label text-white">Image URL</label>
+                    <input type="text" id="inputImg" name="inputImg" className="form-control" placeholder="Enter new image URL" required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="formControlTextarea" className="form-label text-white">Description</label>
+                    <textarea className="form-control" id="formControlTextarea" rows="2" placeholder="Enter new description" minLength="3" maxLength="100" required />
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheck" required />
+                    <label className="form-check-label text-white" htmlFor="flexCheck">
+                        Popular
+                    </label>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center gap-4 mt-2">
+                <button type="submit" className="btn btn-outline-success px-5">Add</button>
+                <button type="button" onClick={handleDelete} className="btn btn-outline-danger px-5">Delete</button>
+            </div>
+        </form>
     )
 }
 
