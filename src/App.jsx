dcd56b6e9038a1fe028477c685d887a2421c656e
gianynx <minusgian@gmail.com>
2025-02-07 12:54:42 +0100

    fix: update form - section id="form-section"

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,10 @@ function App() {
             <section id="technologies-section">
               <div className="p-4">
                 <hr />
-                <h2 className="text-center mt-5">Technologies</h2>
-                <div className="d-flex justify-content-center gap-3 mt-5 mb-2">
+                <div className="d-flex justify-content-center gap-3 mt-5">
                   <CardForm addTechnology={addTechnology} removeTechnology={removeTechnology} />
                 </div>
+                <h2 className="text-center mt-5">Technologies</h2>
                 <div className="d-flex justify-content-center flex-wrap gap-5 mt-5 mb-2">
                   {technologies.map((tech) => (
                     <Card key={tech.id} imgSrc={tech.imgSrc} title={tech.title} isPopular={tech.isPopular}>
@@ -105,21 +105,21 @@ function App() {
                 <div className="d-flex justify-content-center gap-5 mt-5 mb-5">
                   <form className="w-50" onSubmit={handleSubmit}>
                     <div className="mb-3">
-                      <label htmlFor="exampleInputName" className="form-label text-white">Name</label>
-                      <input type="text" id="exampleInputName" name="exampleInputName" className="form-control" onChange={handleChange} minLength="3" maxLength="23" required />
+                      <label htmlFor="inputName" className="form-label text-white">Name</label>
+                      <input type="text" onChange={handleChange} id="inputName" name="inputName" className="form-control" placeholder="Enter your name" minLength="3" maxLength="23" required />
                     </div>
                     <div className="mb-3">
-                      <label htmlFor="exampleInputEmail" className="form-label text-white">Email address</label>
-                      <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="emailHelp" minLength="3" maxLength="23" required />
+                      <label htmlFor="inputEmail" className="form-label text-white">Email address</label>
+                      <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter your email" minLength="3" maxLength="23" required />
                       <div id="emailHelp" className="form-text text-white">We&apos;ll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
-                      <label htmlFor="exampleInputPassword" className="form-label text-white">Password</label>
-                      <input type="password" className="form-control" id="exampleInputPassword" minLength="3" maxLength="23" required />
+                      <label htmlFor="inputPassword" className="form-label text-white">Password</label>
+                      <input type="password" className="form-control" id="inputPassword" placeholder="Enter your password" minLength="3" maxLength="23" required />
                     </div>
                     <div className="mb-3 form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck" required />
-                      <label className="form-check-label text-white" htmlFor="exampleCheck">Check me out</label>
+                      <input type="checkbox" className="form-check-input" id="check" required />
+                      <label className="form-check-label text-white" htmlFor="check">Check me out</label>
                     </div>
                     <button type="submit">Click me!</button>
                   </form>
