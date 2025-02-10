54c1b2f1dbf6aff13c9709e53397f339d2f179af
gianynx <minusgian@gmail.com>
2025-02-10 14:56:23 +0100

    fix: update form

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -39,26 +39,26 @@ function CardForm({ addTechnology, removeTechnology }) {
         <form onSubmit={(e) => { e.preventDefault(); handleAdd() }} className="rounded p-5" style={{ backgroundColor: "#162638" }}>
             <div className="d-flex align-items-center gap-5">
                 <div className="mb-3">
-                    <label htmlFor="inputName" className="form-label text-white">Title</label>
-                    <input type="text" value={formData.title} onChange={handleInputChange} id="inputName" name="inputName" className="form-control" placeholder="Enter new name" minLength="3" maxLength="23" required />
+                    <label htmlFor="title" className="form-label text-white">Title</label>
+                    <input type="text" value={formData.title} onChange={handleInputChange} id="title" name="title" className="form-control" placeholder="Enter new name" minLength="3" maxLength="23" required />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="inputImg" className="form-label text-white">Image URL</label>
-                    <input type="text" value={formData.imgSrc} onChange={handleInputChange} id="inputImg" name="inputImg" className="form-control" placeholder="Enter new image URL" required />
+                    <label htmlFor="imgSrc" className="form-label text-white">Image URL</label>
+                    <input type="text" value={formData.imgSrc} onChange={handleInputChange} id="imgSrc" name="imgSrc" className="form-control" placeholder="Enter new image URL" required />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="formControlTextarea" className="form-label text-white">Description</label>
-                    <textarea value={formData.description} onChange={handleInputChange} className="form-control" id="formControlTextarea" rows="2" placeholder="Enter new description" minLength="3" maxLength="100" required />
+                    <label htmlFor="description" className="form-label text-white">Description</label>
+                    <textarea value={formData.description} onChange={handleInputChange} className="form-control" id="description" name="description" rows="2" placeholder="Enter new description" minLength="3" maxLength="100" required />
                 </div>
                 <div className="form-check">
-                    <input type="checkbox" checked={formData.isPopular} onChange={handleInputChange} className="form-check-input" id="flexCheck" required />
-                    <label className="form-check-label text-white" htmlFor="flexCheck">
+                    <input type="checkbox" checked={formData.isPopular} onChange={handleInputChange} className="form-check-input" id="isPopular" name="isPopular" required />
+                    <label className="form-check-label text-white" htmlFor="isPopular">
                         Popular
                     </label>
                 </div>
                 <div className="form-check">
-                    <input type="checkbox" checked={formData.isJsFramework} onChange={handleInputChange} className="form-check-input" id="flexCheck" required />
-                    <label className="form-check-label text-white" htmlFor="flexCheck">
+                    <input type="checkbox" checked={formData.isJsFramework} onChange={handleInputChange} className="form-check-input" id="isJsFramework" name="isJsFramework" required />
+                    <label className="form-check-label text-white" htmlFor="isJsFramework">
                         JS Framework
                     </label>
                 </div>
