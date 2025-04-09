7e86126e6922667dd521711d876aefae38878738
gianynx <minusgian@gmail.com>
2025-04-09 17:54:01 +0200

    feat: add types

--- src/types/AppHookTypes.ts
+++ src/types/AppHookTypes.ts
@@ -6,6 +6,10 @@ export interface AppHookData {
     technologies: Technology[];
     addTechnology: (newTechnology: Technology) => void;
     removeTechnology: (id: number) => void;
+    formData: FormState;
+    handleInputChange: (field: keyof FormState, value: string | boolean) => void;
+    resetForm: () => void;
+    sendForm: (e: SendFormEvent) => void;
 }
 
 export interface Technology {
@@ -15,5 +19,19 @@ export interface Technology {
     isPopular: boolean;
     description: string;
 }
+
+export interface FormState {
+    email: string;
+    name: string;
+    privacyChecked: boolean;
+}
+
+export interface FormAction {
+    type: "CHANGE_FIELD" | "RESET_FIELD";
+    field?: keyof FormState;
+    value?: string | boolean;
+}
+
+export interface SendFormEvent extends React.FormEvent<HTMLFormElement> { }
 
 export default AppHookData;
