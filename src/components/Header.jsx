57968ccbfecd165cb93f9559773bb6d98d1ac34d
gianynx <minusgian@gmail.com>
2025-04-10 17:43:15 +0200

    feat: add dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,11 +18,65 @@ function Navbar() {
         </div>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav m-auto mb-2 mb-lg-0">
-            <Link>Novità e in evidenza</Link>
-            <Link>Uomo</Link>
-            <Link>Donna</Link>
-            <Link>Kids</Link>
-            <Link>Outlet</Link>
+            <li className="nav-item dropdown">
+              <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link>Novità e in evidenza</Link>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><a className="dropdown-item" href="/">In evidenza</a></li>
+                <li><a className="dropdown-item" href="/">Scopri le icone</a></li>
+                <li><a className="dropdown-item" href="/">Jordan</a></li>
+                <li><a className="dropdown-item" href="/">Scopri lo sport</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link>Uomo</Link>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><a className="dropdown-item" href="/">In evidenza</a></li>
+                <li><a className="dropdown-item" href="/">Scarpe</a></li>
+                <li><a className="dropdown-item" href="/">Abbigliamento</a></li>
+                <li><a className="dropdown-item" href="/">Scopri lo sport</a></li>
+                <li><a className="dropdown-item" href="/">Accessori</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link>Donna</Link>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><a className="dropdown-item" href="/">In evidenza</a></li>
+                <li><a className="dropdown-item" href="/">Scarpe</a></li>
+                <li><a className="dropdown-item" href="/">Abbigliamento</a></li>
+                <li><a className="dropdown-item" href="/">Scopri lo sport</a></li>
+                <li><a className="dropdown-item" href="/">Accessori</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link>Kids</Link>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><a className="dropdown-item" href="/">In evidenza</a></li>
+                <li><a className="dropdown-item" href="/">Scarpe</a></li>
+                <li><a className="dropdown-item" href="/">Abbigliamento</a></li>
+                <li><a className="dropdown-item" href="/">Kids per età</a></li>
+                <li><a className="dropdown-item" href="/">Accessori</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link>Outlet</Link>
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li><a className="dropdown-item" href="/">Offerte</a></li>
+                <li><a className="dropdown-item" href="/">Outlet - Uomo</a></li>
+                <li><a className="dropdown-item" href="/">Outlet - Donna</a></li>
+                <li><a className="dropdown-item" href="/">Outlet - Kids</a></li>
+                <li><a className="dropdown-item" href="/">Outlet per sport</a></li>
+              </ul>
+            </li>
           </ul>
           <div className="d-flex align-items-center gap-1">
             <form className="d-flex mx-2" role="search" onSubmit={(e) => { e.preventDefault() }}>
