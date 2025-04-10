3ab4e66fe9c50e61587324df25561265f32fd825
gianynx <minusgian@gmail.com>
2025-04-10 17:15:33 +0200

    feat: add transition

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import '../assets/css/Link.css';
 
 function Link({ children }) {
 
     return (
         <li className="nav-item px-3">
-            <a className="nav-link" href="/">
+            <a className="nav-link hover-underline" href="/">
                 <span className="fw-medium text-dark">{children}</span>
             </a>
         </li>
