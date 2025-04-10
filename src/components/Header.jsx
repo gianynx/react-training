bb6e37c06d5f3f3626d48d31763ceeab6f1d9bf1
gianynx <minusgian@gmail.com>
2025-04-10 12:16:50 +0200

    wip: update header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import Link from './Link';
 import '../assets/css/Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from "@fortawesome/free-brands-svg-icons";
 
 function Navbar() {
 
@@ -9,24 +11,30 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg bg-white fixed-top px-5 p-2">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <img src="/swoosh.jpg" alt="Nike logo" id="logoNike" />
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div className="container-fluid d-flex justify-content-between">
+        <div>
+          <a className="navbar-brand" href="/">
+            <img src="/swoosh.jpg" alt="Nike logo" id="logoNike" />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <Link>Home</Link>
-            <Link>About</Link>
-            <Link>Services</Link>
-            <Link>Contact</Link>
+          <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+            <Link>Novità e in evidenza</Link>
+            <Link>Uomo</Link>
+            <Link>Donna</Link>
+            <Link>Kids</Link>
+            <Link>Outlet</Link>
           </ul>
           <form className="d-flex mx-5" role="search" onSubmit={handleSubmit}>
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <input className="form-control me-2" type="search" placeholder="Cerca" aria-label="Search" />
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </a>
             <button className="btn btn-dark px-2" type="submit">
-              Click
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </form>
         </div>
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 function Link({ children }) {
 
     return (
-        <li className="nav-item">
-            <button className="nav-link" type="button">
+        <li className="nav-item px-3">
+            <button className="nav-link text-dark" type="button">
                 {children}
             </button>
         </li>
