543f76d48a328f88f255632e833465c76d5f838c
gianynx <minusgian@gmail.com>
2025-05-26 10:04:03 +0200

    feat: update classes

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -3,7 +3,7 @@ import '../assets/css/CardImage.css';
 
 function CardImage({ imgSrc, imgTitle, imgSubTitle, isFullWidth, isCustomLayout }) {
     return (
-        <a href="/" className={`card card-img-container ${isFullWidth ? 'full-width' : ''}`}>
+        <a href="/" className={`card ${isFullWidth ? 'full-width' : 'custom-width'}`}>
             <img src={imgSrc} alt={imgTitle} className="card-img-custom" />
             <div className={`card-img-overlay d-flex ${isCustomLayout ? 'flex-column align-items-end p-4' : 'justify-content-between align-items-baseline p-5'}`}>
                 <h4 className="card-title">{imgTitle}</h4>
