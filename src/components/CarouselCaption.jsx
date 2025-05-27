95a5504af5f4c19525e42809c9f1affa5f1904d0
gianynx <minusgian@gmail.com>
2025-05-27 12:08:19 +0200

    feat: add FontAwesomeIcon & update indicators style

--- src/components/CarouselCaption.jsx
+++ src/components/CarouselCaption.jsx
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBackward, faForward } from '@fortawesome/free-solid-svg-icons';
 
 function CarouselCaption({ media }) {
 
     return (
         <div id="manualCarousel" className="carousel slide">
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
-                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="0" className="active px-2" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="2" aria-label="Slide 3" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="3" aria-label="Slide 4" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="4" aria-label="Slide 5" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="5" aria-label="Slide 6" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="6" aria-label="Slide 7" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="7" aria-label="Slide 8" className="px-2"></button>
+                <button type="button" data-bs-target="#manualCarousel" data-bs-slide-to="8" aria-label="Slide 9" className="px-2"></button>
             </div>
             <div className="carousel-inner">
                 {media.map((item, index) => (
@@ -21,19 +23,17 @@ function CarouselCaption({ media }) {
                         <a href="/">
                             <img src={item.src} className="d-block object-fit-cover mx-auto w-75" alt={item.alt} />
                         </a>
-                        <div className="carousel-caption d-none d-md-block">
+                        <div className="carousel-caption">
                             <h2><span className="badge text-bg-dark">{item.title}</span></h2>
                         </div>
                     </div>
                 ))}
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#manualCarousel" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
+                <FontAwesomeIcon icon={faBackward} className="text-dark fs-2" />
             </button>
             <button className="carousel-control-next" type="button" data-bs-target="#manualCarousel" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
+                <FontAwesomeIcon icon={faForward} className="text-dark fs-2" />
             </button>
         </div>
     );
