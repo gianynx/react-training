2255a004e66edd2658af405aa79f9dab176d6518
gianynx <minusgian@gmail.com>
2025-05-27 10:55:23 +0200

    feat: add tag <a>

--- src/components/CarouselCaption.jsx
+++ src/components/CarouselCaption.jsx
@@ -18,7 +18,9 @@ function CarouselCaption({ media }) {
             <div className="carousel-inner">
                 {media.map((item, index) => (
                     <div key={item.src} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                        <img src={item.src} className="d-block object-fit-cover mx-auto w-75" alt={item.alt} />
+                        <a href="/">
+                            <img src={item.src} className="d-block object-fit-cover mx-auto w-75" alt={item.alt} />
+                        </a>
                         <div className="carousel-caption d-none d-md-block">
                             <h5 className="text-body-secondary">{item.title}</h5>
                         </div>
