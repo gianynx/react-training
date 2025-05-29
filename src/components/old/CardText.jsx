4d482bd83fce16bcd319a542f969cfa43480e9d4
gianynx <minusgian@gmail.com>
2025-05-29 12:33:22 +0200

    fix: move CardText.jsx & CardText.css into folders "old"

--- src/components/old/CardText.jsx
+++ src/components/old/CardText.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import Toast from './Toast';
-import '../assets/css/CardText.css';
+import Toast from '../Toast';
+import '../../assets/css/old/CardText.css';
 
 function CardText({ id, imgSrc, title, children, isPopular, removeTechnology }) {
 
